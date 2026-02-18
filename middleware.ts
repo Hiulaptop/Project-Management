12982d3e99d78d57c53370ae3fe981c5d79588f5
Hiulaptop <tlthieu200806@gmail.com>
2026-02-18 11:55:47 +0700

    Add middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,8 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  
-  return NextResponse.next()
+    console.log("HIHI")
+    return NextResponse.next()
 }
 
 export const config = {
